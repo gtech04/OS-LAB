e52bc134a4e96e826e9086a362a4e9cfcf4a2dca
Georgy Thomas Geo <georgythomas04@gmail.com>
2025-01-29 23:06:28 +0530

    Add files via upload

---
+++ Shared_receiver.c
@@ -0,0 +1,16 @@
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+int main()
+{
+void *shared_memory;
+char buffer[100];
+int shmid;
+shmid= shmget((key_t)1222,1024,0666);
+printf("Key of the Shared Memory is %d\n",shmid); shared_memory=shmat(shmid,NULL,0);
+printf("Process attached at %p\n",shared_memory); printf("Data read from shared memory is : %s\n",(char *)shared_memory);
+return 0;
+}
---
+++ Shared_sender.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+int main()
+{
+void *shared_memory;
+char buffer[100];
+int shmid;
+shmid= shmget((key_t)1222,1024,0666|IPC_CREAT); printf("Key of the Shared Memory is %d\n",shmid); shared_memory=shmat(shmid,NULL,0);
+printf("Process attached at %p\n",shared_memory); printf("Enter some data to write to shared memory: \n"); read(0,buffer,100);
+strcpy(shared_memory,buffer);
+printf("You wrote is: %s",(char *)shared_memory); return 0;
+}
