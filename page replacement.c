5612edf49271a211edade5df32cfbc6d10e72a9f
Georgy Thomas Geo <georgythomas04@gmail.com>
2025-03-06 07:38:59 +0530

    page replacement.c

--- page replacement.c
+++ page replacement.c
@@ -1,16 +1,19 @@
 //FIFO
-
-#include <stdio.h>
+#include<stdio.h>
 void main()
 {
-int i,j,n,a[50],frame[10],no,k,avail,count=0;
-printf("\n ENTER THE NUMBER OF PAGES:\n"); scanf("%d",&n);
-printf("\n ENTER THE PAGE NUMBER :\n"); for(i=1;i<=n;i++)
+int i,j,n,a[50],frame[10],no,k,avail,count=0; printf("\n ENTER THE NUMBER OF PAGES:\n");
+scanf("%d",&n);
+printf("\n ENTER THE PAGE NUMBER :\n");
+for(i=1;i<=n;i++)
 scanf("%d",&a[i]);
-printf("\n ENTER THE NUMBER OF FRAMES :"); scanf("%d",&no);
-for(i=0;i<no;i++) frame[i]= -1;
-j=0;printf("ref string\t page frames\n");
-printf("___________________________________\n");
+printf("\n ENTER THE NUMBER OF FRAMES :");
+scanf("%d",&no);
+for(i=0;i<no;i++)
+frame[i]= -1;
+j=0;
+printf("ref string\t page frames\n");
+printf("______________________________ _____\n");
 for(i=1;i<=n;i++)
 {
 printf("%d\t\t",a[i]);
@@ -28,8 +31,7 @@ if (avail==0)
 frame[j]=a[i];
 j=(j+1)%no;
 count++;
-for(k=0;k<no;k++)
-if(frame[k]!=-1)
+for(k=0;k<no;k++) if(frame[k]!=-1)
 printf("%d\t",frame[k]);
 }
 printf("\n");
@@ -40,88 +42,95 @@ printf("Page Fault Is %d\n",count);
 //LRU
 
 #include <stdio.h>
+#include <limits.h>
 
-int fr[10]; // Frame array
-
-void display(int frsize) {
-    printf("\nFrames: ");
+// Function to display the current frames
+void display(int fr[], int frsize) {
+    printf("\n");
     for (int i = 0; i < frsize; i++) {
         if (fr[i] != -1)
-            printf("%d ", fr[i]);
+            printf("\t%d", fr[i]);
         else
-            printf("- ");
+            printf("\t-");
     }
-    printf("\n");
+}
+
+// Function to find the least recently used (LRU) page
+int find_LRU(int timestamps[], int size) {
+    int min_time = INT_MAX, index = -1;
+    for (int i = 0; i < size; i++) {
+        if (timestamps[i] < min_time) {
+            min_time = timestamps[i];
+            index = i;
+        }
+    }
+    return index;
 }
 
 int main() {
-    int n, frsize, i, j, index, min, pf = 0;
+    int n, frsize;
     
-    printf("Enter the number of page references: ");
+    // User input for number of pages
+    printf("Enter the number of pages: ");
     scanf("%d", &n);
     
     int p[n]; // Page reference string
     printf("Enter the page reference string: ");
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &p[i]);
     }
     
+    // User input for number of frames
     printf("Enter the number of frames: ");
     scanf("%d", &frsize);
-
-    // Initialize frames to -1 (empty)
-    for (i = 0; i < frsize; i++) {
+    
+    int fr[frsize]; // Frame array
+    int timestamps[frsize];
+    int time = 0, pf = 0;
+    
+    // Initialize frame array
+    for (int i = 0; i < frsize; i++) {
         fr[i] = -1;
+        timestamps[i] = 0;
     }
-
-    int recent[10]; // Tracks the last used time of each page
-
-    for (j = 0; j < n; j++) {
-        int flag1 = 0, flag2 = 0;
-
-        // Check if the page is already in frames
-        for (i = 0; i < frsize; i++) {
+    
+    for (int j = 0; j < n; j++) {
+        int flag1 = 0;
+        
+        // Check if the page is already in the frame
+        for (int i = 0; i < frsize; i++) {
             if (fr[i] == p[j]) {
                 flag1 = 1;
-                flag2 = 1;
-                recent[i] = j; // Update recent usage
+                timestamps[i] = ++time; // Update timestamp since it's used recently
                 break;
             }
         }
-
-        // If the page is NOT in memory
+        
+        // If not found, page fault occurs
         if (flag1 == 0) {
-            for (i = 0; i < frsize; i++) {
+            int replace_index = -1;
+            
+            // Look for an empty slot first
+            for (int i = 0; i < frsize; i++) {
                 if (fr[i] == -1) { // Empty slot found
-                    fr[i] = p[j];
-                    recent[i] = j;
-                    flag2 = 1;
-                    pf++; // Page fault
+                    replace_index = i;
                     break;
                 }
             }
-        }
-
-        // If no empty frame, find LRU page and replace it
-        if (flag2 == 0) {
-            min = recent[0];
-            index = 0;
-
-            for (i = 1; i < frsize; i++) {
-                if (recent[i] < min) {
-                    min = recent[i];
-                    index = i;
-                }
+            
+            // If no empty slot, replace the LRU page
+            if (replace_index == -1) {
+                replace_index = find_LRU(timestamps, frsize);
             }
-
-            fr[index] = p[j];
-            recent[index] = j;
-            pf++; // Page fault
+            
+            fr[replace_index] = p[j];  // Replace page
+            timestamps[replace_index] = ++time;
+            pf++; // Increment page fault count
         }
-
-        display(frsize);
+        
+        display(fr, frsize);
     }
-
+    
     printf("\nTotal Page Faults: %d\n", pf);
     return 0;
 }
